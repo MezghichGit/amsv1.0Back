ed4dd10f99577339f7244cd5b79430f9c144a354
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-07 20:07:49 +0100

    crud Provider

--- src/main/java/com/sip/ams/AmsApiApplication.java
+++ src/main/java/com/sip/ams/AmsApiApplication.java
@@ -3,11 +3,22 @@ package com.sip.ams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AmsApiApplication {
+import com.sip.ams.services.FilesStorageService;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class AmsApiApplication implements CommandLineRunner {
+	 @Resource
+	  FilesStorageService storageService;
 	public static void main(String[] args) {
 		SpringApplication.run(AmsApiApplication.class, args);
 	}
-
+	@Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
 }
--- src/main/java/com/sip/ams/entities/Provider.java
+++ src/main/java/com/sip/ams/entities/Provider.java
@@ -10,55 +10,77 @@ import javax.validation.constraints.NotBlank;
 @Entity
 public class Provider {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @NotBlank(message = "Name is mandatory")
-    @Column(name = "name")
-    private String name;
-    
-    @NotBlank(message = "Address is mandatory")
-    @Column(name = "address")
-    private String address;
-    
-    @NotBlank(message = "Email is mandatory")
-    @Column(name = "email")
-    private String email;
+	@NotBlank(message = "Name is mandatory")
+	@Column(name = "name")
+	private String name;
 
-    public Provider() {}
+	@NotBlank(message = "Address is mandatory")
+	@Column(name = "address")
+	private String address;
 
-    public Provider(String name, String address, String email) {
-        this.name = name;
-        this.address = address;
-        this.email = email;
-    }
+	@NotBlank(message = "Email is mandatory")
+	@Column(name = "email")
+	private String email;
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	@Column(name = "picByte", length = 3000)
+	private byte[] picByte;
 
-    public long getId() {
-        return id;
-    }
+	public Provider() {
+	}
 
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public Provider(String name, String address, String email, byte[] picByte) {
+		this.name = name;
+		this.address = address;
+		this.email = email;
+		this.picByte = picByte;
 
-    public String getAddress() {
-        return address;
-    }
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public byte[] getPicByte() {
+
+		return picByte;
+
+	}
+
+	public void setPicByte(byte[] picByte) {
+
+		this.picByte = picByte;
+
+	}
+
 }
